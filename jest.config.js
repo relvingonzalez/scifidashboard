594c76eb0f3db65b73d63c51c9c6b67dc95ba60d
Relvin Gonzalez <otsanllc@gmail.com>
2025-01-22 23:47:14 -0600

    Small fixes

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    ...require('react-scripts/config/jest/jest-config'),
-    transform: {},
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
-    extensionsToTreatAsEsm: ['.js', '.jsx', '.ts', '.tsx'],
-    testPathIgnorePatterns: ["/node_modules/", "**/node_modules/"]
-  };
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -14,7 +14,7 @@ import Button from '../Button/Button';
 const Reports = () => (
   <>
     <div className={styles.featuredCharts}>
-      <GreetingCard name="Ray" />
+      <GreetingCard name="traveler" />
       <Card title="Battles" subtitle="Per Month">
         <LineChart datasets={lineDataSets} />
       </Card>
