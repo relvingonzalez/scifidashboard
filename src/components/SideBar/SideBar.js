c511d0420f24b047724bb0f2a4e8c6e7d3776fd1
Relvin Gonzalez <otsanllc@gmail.com>
2025-01-22 21:52:57 -0600

    togle fix

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './SideBar.module.css';
 import Button from '../Button/Button';
+import { ChevronsLeft, ChevronsRight } from 'lucide-react';
 
 const SideBar = ({ isOpen, onToggleOpen, children }) => (
     <nav className={`${styles.SideBar} ${isOpen && styles.open}`} data-testid="SideBar">
       <Button className={`font-lg ${styles.toggle}`} onClick={() => onToggleOpen(!isOpen)}>
-        <p className={`${isOpen && 'rotate'}`}>+</p>
+        {isOpen ? <ChevronsLeft /> : <ChevronsRight />}
       </Button>
       <h1 className="brand">{isOpen ? 'HAL 9000' : 'H'}</h1>
       <ul>
