6d0c91d2aad1df32791dc4c31cfc0f8aeb80a649
Relvin Gonzalez <otsanllc@gmail.com>
2025-01-22 20:07:25 -0600

    Toggle button

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,7 +4,9 @@ import Button from '../Button/Button';
 
 const SideBar = ({ isOpen, onToggleOpen, children }) => (
     <nav className={`${styles.SideBar} ${isOpen && styles.open}`} data-testid="SideBar">
-      <Button variant="unstyled" className={`font-lg ${isOpen && 'rotate'} ${styles.Toggle}`} onClick={() => onToggleOpen(!isOpen)}>+</Button>
+      <Button variant="unstyled" className={`font-lg ${styles.toggle}`} onClick={() => onToggleOpen(!isOpen)}>
+        <p className={`${isOpen && 'rotate'}`}>+</p>
+      </Button>
       <h1 className="brand">{isOpen ? 'HAL 9000' : 'H'}</h1>
       <ul>
         {children}
